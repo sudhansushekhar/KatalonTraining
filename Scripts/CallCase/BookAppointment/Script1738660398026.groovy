9b1e2cc0b4ee4cd70c14e3d132bbdb3e59840dc0
sudhansushekhar <sudhanshushekhar496@gmail.com>
2025-02-04 14:46:37 +0530

    call case

---
+++ Scripts/CallCase/BookAppointment/Script1738660398026.groovy
@@ -0,0 +1,56 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+//Call Case- login to proceed after login
+WebUI.callTestCase(findTestCase('CallCase/LoginCURA'), [:], FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_CURA Healthcare Service/Tokyo CURA Healthcare Center'), 
+    'Hongkong CURA Healthcare Center', true)
+
+WebUI.check(findTestObject('Object Repository/Page_CURA Healthcare Service/Apply for hospital readmission'))
+
+WebUI.verifyElementChecked(findTestObject('Object Repository/Page_CURA Healthcare Service/Apply for hospital readmission'), 
+    5, FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementNotChecked(findTestObject('Object Repository/Page_CURA Healthcare Service/radio_Medicaid'), 5, FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/radio_Medicaid'))
+
+WebUI.verifyElementChecked(findTestObject('Object Repository/Page_CURA Healthcare Service/radio_Medicaid'), 5, FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/Visit Date (Required)'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/td_12'))
+
+WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/Comment'), 'test')
+
+WebUI.verifyElementClickable(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Book Appointment'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Book Appointment'))
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_CURA Healthcare Service/Appointment Confirmation'), 2)
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Go to Homepage'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/menu-toggle'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Logout'))
+
+WebUI.closeBrowser()
+
--- Scripts/Frames/iframe_TC01/Script1738596067984.groovy
+++ Scripts/Frames/iframe_TC01/Script1738596067984.groovy
@@ -1,24 +1,8 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable
-
 import static org.junit.Assert.assertEquals
 
-import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
 
 WebUI.openBrowser('')
 WebUI.maximizeWindow()
@@ -33,8 +17,9 @@ WebUI.click(findTestObject('Object Repository/Frames/TryItBtn'))
 String alertTextActual = WebUI.getAlertText()
 String alertTextExp = "Hello! I am an alert box!"
 
-assertEquals(alertTextActual, alertTextExp)
-
+//if you don't want to run any particular test step add text "not_run:" at the beginning to the step
+not_run: assertEquals(alertTextActual, alertTextExp)  //disabled step
+assertEquals(alertTextActual, alertTextExp)  //enabled step
 //Accept alert
 
 WebUI.acceptAlert()
